e0c43442107fa57a8ba6a0fc4a449228ff8471fc
RubenNH <104938082+RubenNH@users.noreply.github.com>
2024-01-12 10:13:57 -0600

    first commit

---
+++ src/components/sideBar.vue
@@ -0,0 +1,26 @@
+<template>
+    <b-sidebar no-header-close no-close-on-backdrop visible id="sidebar-1" title="Sidebar" shadow >
+        <div class="px-3 py-2">
+        <p>
+            Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis
+            in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+        </p>
+        <b-img src="https://picsum.photos/500/500/?image=54" fluid thumbnail></b-img>
+        </div>
+    </b-sidebar>
+</template>
+
+<script>
+export default {
+  name: 'Sidebar',
+};
+</script>
+
+<!-- <style scoped>
+.sidebar {
+  width: 200px;
+  background-color: #333;
+  color: #fff;
+  padding: 10px;
+}
+</style> -->
