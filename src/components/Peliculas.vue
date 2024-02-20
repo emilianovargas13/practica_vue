ee392a618dd4ff34b2ed70b2d515de7c9a2d60a2
RubenNH <104938082+RubenNH@users.noreply.github.com>
2024-02-20 14:32:02 -0600

    changes

--- src/components/Peliculas.vue
+++ src/components/Peliculas.vue
@@ -1,115 +1,162 @@
 <template>
-    <b-container fluid>
-  
-      <b-row>
-        <b-button class="mt-3" size="lg" @click="openModal">Registrar película</b-button>
-        <createNewPeli :movies="movies" />
-  
-        <b-modal id="modal-1" title="Registro de películas" v-model="showModal">
-          <b-form @submit="onSubmit" @reset="onReset">
-            <b-form-group id="input-group-1" label="Nombre de película:" label-for="input-1" description="Registra el nombre latinoamericano.">
-              <b-form-input id="input-1" v-model="form.name" type="text" placeholder="Ingresa el nombre de la película." required></b-form-input>
-            </b-form-group>
-  
-            <b-form-input id="input-2" v-model="form.year" type="number" required placeholder="Ingresa el año de la película"></b-form-input>
-  
-            <b-form-group id="input-group-4" label="Género" label-for="input-3">
-              <b-form-select v-model="form.genre" :options="genres" required>
-                <template v-slot:first>
-                  <option :value="0">Selecciona un género</option>
-                </template>
-              </b-form-select>
-              <div v-if="genres.length === 0">Loading genres...</div>
-            </b-form-group>
-            <b-button type="submit" variant="primary">Registrar</b-button>
-            
-          </b-form>
-        </b-modal>
-      </b-row>
-     
-    </b-container>
-  </template>
+  <b-container fluid>
+    <b-row>
+      <createNewPeli :genres="genres" :genresToSend="genresToSend" :movies="movies"/>
+      <b-button class="mt-3" size="lg" v-b-modal.modalon>Registrar película</b-button>
+      <b-card>
+        <b-card-header>
+          <h3 class="text-center">Peliculas</h3>
+        </b-card-header>
+        <b-card-body>
+          <!-- card where are gonna to show the filter options. search by name, directos, range of years, genres and by publish date -->
+          <b-card>
+            <b-card-body>
+              <b-form @submit="onSubmit">
+                <b-row>
+                  <b-col cols="12" md="4">
+                    <b-form-group label="Nombre">
+                      <b-form-input v-model="seachForm.name" />
+                    </b-form-group>
+                  </b-col>
+                  <b-col cols="12" md="4">
+                    <b-form-group label="Director">
+                      <b-form-input v-model="seachForm.director" />
+                    </b-form-group>
+                  </b-col>
+                  <b-col cols="12" md="4">
+                    <b-form-group label="Genero">
+                      <b-form-select v-model="seachForm.genre" :options="genres" style="width: 100%">
+                        <template v-slot:first>
+                          <option :value="0">Ninguna</option>
+                        </template>
+                      </b-form-select>
+                    </b-form-group>
+                  </b-col>
+                </b-row>
+                <b-row>
+                  <b-col cols="12" md="4">
+                    <b-form-group label="Fecha de publicación">
+                      <b-datepicker v-model="seachForm.publishDate"></b-datepicker>
+                    </b-form-group>
+                  </b-col>
+                  <b-col cols="12" md="4">
+                    <b-form-group label="Año Inicio">
+                      <b-form-input v-model="seachForm.yearIni" type="number"></b-form-input>
+                    </b-form-group>
+                  </b-col>
+                  <b-col cols="12" md="4">
+                    <b-form-group label="Año Final">
+                      <b-form-input v-model="seachForm.yearEnd" type="number"></b-form-input>
+                    </b-form-group>
+                  </b-col>
+
+                  <b-col cols="12" md="12" class="mt-4">
+                    <b-button type="submit" variant="primary" style="width: 100%;">Buscar</b-button>
+                  </b-col>
+                </b-row>
+              </b-form>
+            </b-card-body>
+          </b-card>
+
+          <b-table striped hover :items="movies" :fields="fields" />
+        </b-card-body>
+      </b-card>
+    </b-row>
+
+  </b-container>
+</template>
     
-  <script>
-  import axios from 'axios';
-  import createNewPeli from './modals/createNewPeli.vue';
-  export default {
-    components: { createNewPeli },
-    data() {
-      return {
-        form: {
-          name: '',
-          genre: '',
-          year: '',
-        },
-        genres: [],
-        genresToSend: [],
-        movies: [], 
-        showModal: false,
-      };
+<script>
+import axios from 'axios';
+import createNewPeli from './modals/createNewPeli.vue';
+export default {
+  components: { createNewPeli },
+  data() {
+    return {
+      genres: [],
+      genresToSend: [],
+      movies: [],
+      fiveYearsOnFuture: '',
+      fields: [
+        { key: 'name', label: 'Nombre', sortable: true },
+        { key: 'genre', label: 'Genero', sortable: true },
+        { key: 'year', label: 'Año', sortable: true },
+        { key: 'director', label: 'Director', sortable: true },
+        { key: 'publishDate', label: 'Fecha de publicación', sortable: true },
+      ],
+      seachForm: {
+        name: '',
+        director: '',
+        yearIni: '',
+        yearEnd: '',
+        genre: '',
+        publishDate: '',
+      },
+    };
+  },
+  mounted() {
+    this.fetchGenres();
+    this.fetchMovies();
+  },
+  methods: {
+    async fetchGenres() {
+      try {
+        const response = await axios.get('http://localhost:8080/api/movies/genres');
+        this.genres = response.data.map(genre => ({ text: genre.name, value: genre.id }));
+        this.genresToSend = response.data;
+      } catch (error) {
+        console.error('Error al obtener los géneros:', error);
+      }
     },
-    mounted() {
-      this.fetchGenres();
-      this.fetchMovies();
-      this.form.genre = 0;
+    async fetchMovies() {
+      try {
+        const response = await axios.get('http://localhost:8080/api/movies/movies');
+        this.movies = response.data;
+        this.movies.forEach(movie => {
+          movie.genre = movie.genre.name;
+        });
+      } catch (error) {
+        console.error('Error al obtener las películas:', error);
+      }
     },
-    methods: {
-      openModal() {
-        this.showModal = true;
-      },
-      onSubmit(evt) {
-        evt.preventDefault();
-        this.registerMovie();
-      },
-      onReset(evt) {
-        this.form.name = '';
-        this.form.genre = '';
-        this.form.year = '';
-        this.showModal = false;
-      },
-      async fetchGenres() {
-        try {
-          const response = await axios.get('http://localhost:8080/api/movies/genres');
-          this.genres = response.data.map(genre => ({ text: genre.name, value: genre.id }));
-          this.genresToSend = response.data;
-        } catch (error) {
-          console.error('Error al obtener los géneros:', error);
+    async onSubmit(evt) {
+      evt.preventDefault();
+      try {
+
+        //search genre by the from and send the name of the genre
+        if (this.seachForm.genre > 0) {
+          this.seachForm.genre = this.genresToSend.find(genre => genre.id === this.seachForm.genre);
+          this.seachForm.genre = this.seachForm.genre.name;
+        }else{
+          this.seachForm.genre = '';
         }
-      },
-      async fetchMovies() {
-        try {
-          const response = await axios.get('http://localhost:8080/api/movies/movies');
-          this.movies = response.data;
-          this.movies.forEach(movie => {
-            movie.genre = movie.genre.name;
-          });
-        } catch (error) {
-          console.error('Error al obtener las películas:', error);
+        if (this.seachForm.publishDate) {
+          this.seachForm.publishDate = new Date(this.seachForm.publishDate).toISOString().split('T')[0];
+          this.seachForm.publishDate = this.seachForm.publishDate.replace(/-/g, '/');
         }
-      },
-      async registerMovie() {
-        try {
-            if (this.form.name && this.form.genre && this.form.year) {
-  
-            this.form.genre = this.genresToSend.find(genre => genre.id === this.form.genre);
-  
-            // Enviar los datos al servidor
-            await axios.post('http://localhost:8080/api/movies/movies', {
-              name: this.form.name,
-              genre: this.form.genre,
-              year: this.form.year,
-            });
-  
-            alert('¡Película registrada con éxito!');
-            this.onReset(); // Limpiar el formulario después de registrar la película
-            this.fetchMovies(); // Recargar las películas después de registrar una nueva
-          };
-        } catch (error) {
-          console.error('Error al registrar la película:', error);
-        }
-      },
-  
-    }
-  };
-  </script>
+        this.fiveYearsOnFuture = new Date();
+        this.fiveYearsOnFuture.setFullYear(this.fiveYearsOnFuture.getFullYear() + 5);
+        this.fiveYearsOnFuture = this.fiveYearsOnFuture.toISOString().split('T')[0];
+        this.fiveYearsOnFuture = this.fiveYearsOnFuture.replace(/-/g, '/');
+
+        const response = await axios.post('http://localhost:8080/api/movies/search', { 
+          name: this.seachForm.name,
+          director: this.seachForm.director,
+          year1: this.seachForm.yearIni,
+          year2: this.seachForm.yearEnd,
+          genre: this.seachForm.genre,
+          publishDate: this.seachForm.publishDate ? this.seachForm.publishDate : this.fiveYearsOnFuture,
+        });
+        this.movies = response.data;
+        this.movies.forEach(movie => {
+          movie.genre = movie.genre.name;
+        });
+      } catch (error) {
+        console.error('Error al buscar las películas:', error);
+      }
+    },
+
+  }
+};
+</script>
   
--- src/components/modals/createNewPeli.vue
+++ src/components/modals/createNewPeli.vue
@@ -1,24 +1,111 @@
 <template>
-      <b-card>
-        <b-card-header>
-          <h3 class="text-center">Peliculas</h3>
-        </b-card-header>
-        <b-card-body>
-          <b-modal v-model="showModal" title="Agregar Pelicula">
-            <createNewPeli @onReset="onReset" @onSubmit="onSubmit" :genres="genresToSend" />
-          </b-modal>
-          <b-table striped hover :items="movies" :fields="fields" />
-        </b-card-body>
-      </b-card>
-  </template>
+  <b-modal id="modalon" title="Registro de películas" hide-footer>
+    <b-form @submit="onSubmit2" @reset="onReset">
+      <b-form-group id="input-group-1" label="Nombre de película:" label-for="input-1"
+        description="Registra el nombre latinoamericano.">
+        <b-form-input id="input-1" v-model="form.name" type="text" placeholder="Ingresa el nombre de la película."
+          required></b-form-input>
+      </b-form-group>
+
+      <b-form-input id="input-2" v-model="form.year" type="number" required
+        placeholder="Ingresa el año de la película"></b-form-input>
+
+      <b-form-group id="input-group-4" label="Género" label-for="input-3" class="mb-2">
+        <b-form-select v-model="form.genre" :options="genres" required style="width: 100%">
+          <template v-slot:first>
+            <option :value="0">Selecciona un género</option>
+          </template>
+        </b-form-select>
+        <div v-if="genres.length === 0">Loading genres...</div>
+      </b-form-group>
+      <b-form-input type="text" v-model="form.director" class="mb-2" required
+        placeholder="Ingresa el nombre del director"></b-form-input>
+      <b-datepicker id="input-3" v-model="form.publishDate" class="mb-2" required
+        placeholder="Ingresa la fecha de estreno"></b-datepicker>
+      <b-button type="submit" variant="primary">Registrar</b-button>
+
+    </b-form>
+  </b-modal>
+</template>
   
-  <script>
-  export default {
-    props: ['movies'],
-  };
-  </script>
+<script>
+export default {
+  props: {
+    genres: Array,
+    genresToSend: Array,
+    movies: Array,
+  },
+  data() {
+    return {
+      fields: [
+        { key: 'name', label: 'Nombre', sortable: true },
+        { key: 'genre', label: 'Genero', sortable: true },
+        { key: 'year', label: 'Año', sortable: true },
+        { key: 'director', label: 'Director', sortable: true },
+        { key: 'publishDate', label: 'Fecha de publicación', sortable: true },
+      ],
+      form: {
+        name: '',
+        genre: '',
+        year: '',
+        director: '',
+        publishDate: '',
+      },
+    };
+  },
+  methods: {
+    onSubmit2(evt) {
+      evt.preventDefault();
+      this.$emit('onSubmit', this.form);
+      this.form = {
+        name: '',
+        genre: '',
+        year: '',
+        director: '',
+        publishDate: '',
+      };
+    },
+    onReset(evt) {
+      evt.preventDefault();
+      this.form = {
+        name: '',
+        genre: '',
+        year: '',
+        director: '',
+        publishDate: '',
+      };
+    },
+    async registerMovie() {
+      try {
+        if (this.form.name && this.form.genre && this.form.year) {
+
+          this.form.genre = this.genresToSend.find(genre => genre.id === this.form.genre);
+
+          // Enviar los datos al servidor
+          await axios.post('http://localhost:8080/api/movies/movies', {
+            name: this.form.name,
+            genre: this.form.genre,
+            year: this.form.year,
+            director: this.form.director,
+            publishDate: this.form.publishDate,
+          });
+
+          alert('¡Película registrada con éxito!');
+          this.onReset(); // Limpiar el formulario después de registrar la película
+          this.fetchMovies(); // Recargar las películas después de registrar una nueva
+        };
+      } catch (error) {
+        console.error('Error al registrar la película:', error);
+      }
+    },
+
+  },
+  mounted() {
+    this.form.genre = 0;
+  },
+
+};
+</script>
   
-  <style scoped>
-  /* Estilos adicionales según sea necesario */
-  </style>
+<style scoped>/* Estilos adicionales según sea necesario */</style>
   
